85e97052c5de6ce400245947ac7b7cdf5aa7a0e8
Galih Muhammad R <galihmramdan17@gmail.com>
2023-07-09 14:59:25 +0700

    Update decoders.dart

--- lib/config/decoders.dart
+++ lib/config/decoders.dart
@@ -1,44 +1,13 @@
-import '/app/models/web_view_target.dart';
+import 'package:flutter_app/app/models/food_recipe.dart';
 import 'package:flutter_app/app/models/user.dart';
 import 'package:flutter_app/app/networking/dio/base_api_service.dart';
 import '/app/networking/api_service.dart';
 
-/*
-|--------------------------------------------------------------------------
-| Model Decoders
-| -------------------------------------------------------------------------
-| Model decoders are used in 'app/networking/' for morphing json payloads
-| into Models.
-|
-| Learn more https://nylo.dev/docs/5.x/decoders#model-decoders
-|--------------------------------------------------------------------------
-*/
-
 final Map<Type, dynamic> modelDecoders = {
-  List<User>: (data) => List.from(data).map((json) => User.fromJson(json)).toList(),
-  //
   User: (data) => User.fromJson(data),
-
-  // User: (data) => User.fromJson(data),
-
-  List<WebViewTarget>: (data) => List.from(data).map((json) => WebViewTarget.fromJson(json)).toList(),
-
-  WebViewTarget: (data) => WebViewTarget.fromJson(data),
+  FoodRecipe: (data) => FoodRecipe.fromJson(data),
 };
-
-/*
-|--------------------------------------------------------------------------
-| API Decoders
-| -------------------------------------------------------------------------
-| API decoders are used when you need to access an API service using the
-| 'api' helper. E.g. api<MyApiService>((request) => request.fetchData());
-|
-| Learn more https://nylo.dev/docs/5.x/decoders#api-decoders
-|--------------------------------------------------------------------------
-*/
 
 final Map<Type, BaseApiService> apiDecoders = {
   ApiService: ApiService(),
-
-  // ...
 };
