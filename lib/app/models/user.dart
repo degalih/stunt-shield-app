a2116a85f571480161886f5702ca485f3501a70f
Galih Muhammad R <galihmramdan17@gmail.com>
2023-07-09 14:59:22 +0700

    Update user.dart

--- lib/app/models/user.dart
+++ lib/app/models/user.dart
@@ -1,15 +1,31 @@
 import 'package:nylo_framework/nylo_framework.dart';
 
 class User extends Model {
-  String? name;
+  int? id;
+  String? firstName;
+  String? lastName;
   String? email;
+  String? username;
+  String? provider;
+  String? jwt;
 
   User();
 
   User.fromJson(dynamic data) {
-    name = data['name'];
-    email = data['email'];
+    this.firstName = data['user']['firstName'];
+    this.lastName = data['user']['lastName'];
+    this.email = data['user']['email'];
+    this.username = data['user']['username'];
+    this.provider = data['user']['provider'];
+    this.jwt = data['jwt'];
   }
 
-  toJson() => {"name": name, "email": email};
+  toJson() => {
+        "firstName": this.firstName,
+        "lastName": this.lastName,
+        "email": this.email,
+        "username": this.username,
+        "provider": this.provider,
+        "jwt": this.jwt,
+      };
 }
