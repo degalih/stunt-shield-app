cdcbf40aef03e9dd674ae08d0dbaccb74d743af3
Galih Muhammad R <galihmramdan17@gmail.com>
2023-07-25 09:40:25 +0700

    consume get all recipe

--- lib/config/decoders.dart
+++ lib/config/decoders.dart
@@ -1,11 +1,13 @@
-import 'package:flutter_app/app/models/food_recipe.dart';
+import 'package:flutter_app/app/models/food_recipe_list.dart';
 import 'package:flutter_app/app/models/user.dart';
 import 'package:flutter_app/app/networking/dio/base_api_service.dart';
 import '/app/networking/api_service.dart';
 
 final Map<Type, dynamic> modelDecoders = {
   User: (data) => User.fromJson(data),
-  FoodRecipe: (data) => FoodRecipe.fromJson(data),
+  // FoodRecipe: (data) => FoodRecipe.fromJson(data)
+  List<FoodRecipeList>: (data) => List<FoodRecipeList>.from(
+      data["data"].map((x) => FoodRecipeList.fromJson(x))),
 };
 
 final Map<Type, BaseApiService> apiDecoders = {
