82ad59c978167c25c337d0911f54bbd1a8ffb972
Galih Muhammad R <galihmramdan17@gmail.com>
2023-07-25 12:10:32 +0700

    create custom back button widget

---
+++ lib/resources/widgets/custom_back_button_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/bootstrap/helpers.dart';
+
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: ThemeColor.get(context).white,
+      surfaceTintColor: ThemeColor.get(context).white,
+      elevation: 4,
+      child: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: Icon(
+          CupertinoIcons.back,
+          color: ThemeColor.get(context).green,
+        ),
+      ),
+      shape: const BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(16),
+        ),
+      ),
+    );
+  }
+}
