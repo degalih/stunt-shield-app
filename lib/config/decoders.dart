b4a703bd2585e326374c53f9b80d1e6af59b3bc9
Galih Muhammad R <galihmramdan17@gmail.com>
2023-08-04 02:11:51 +0700

    add decoder for bookmark and profile

--- lib/config/decoders.dart
+++ lib/config/decoders.dart
@@ -1,3 +1,7 @@
+import 'package:flutter_app/app/models/response/favorite_id.dart';
+import 'package:flutter_app/app/models/response/food_recipe_favorite.dart';
+import 'package:flutter_app/app/models/response/profile_me.dart';
+
 import '../app/models/response/food_recipe_search_result.dart';
 import '../app/models/response/food_recipe_detail.dart';
 import 'package:flutter_app/app/models/response/food_recipe_list.dart';
@@ -7,19 +11,15 @@ import '/app/networking/api_service.dart';
 
 final Map<Type, dynamic> modelDecoders = {
   User: (data) => User.fromJson(data),
-  // FoodRecipe: (data) => FoodRecipe.fromJson(data)
+  FoodRecipeDetail: (data) => FoodRecipeDetail.fromJson(data),
+  FavoriteId: (data) => FavoriteId.fromJson(data),
+  ProfileMe: (data) => ProfileMe.fromJson(data),
   List<FoodRecipeList>: (data) => List<FoodRecipeList>.from(
       data["data"].map((x) => FoodRecipeList.fromJson(x))),
-
-  // List<FoodRecipeDetail>: (data) =>
-  //     List.from(data).map((json) => FoodRecipeDetail.fromJson(json)).toList(),
-
-  FoodRecipeDetail: (data) => FoodRecipeDetail.fromJson(data),
-
   List<FoodRecipeSearchResult>: (data) => List<FoodRecipeSearchResult>.from(
       data["data"].map((x) => FoodRecipeSearchResult.fromJson(x))),
-
-  // FoodRecipeSearchResult: (data) => FoodRecipeSearchResult.fromJson(data),
+  List<FoodRecipeFavorite>: (data) => List<FoodRecipeFavorite>.from(
+      data["data"].map((x) => FoodRecipeFavorite.fromJson(x))),
 };
 
 final Map<Type, BaseApiService> apiDecoders = {
