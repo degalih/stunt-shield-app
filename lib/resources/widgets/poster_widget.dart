de83a9daff3a636592fe7a949cae3ae7c0dcf9f8
Galih Muhammad R <galihmramdan17@gmail.com>
2023-08-15 15:11:52 +0700

    Create poster_widget.dart

---
+++ lib/resources/widgets/poster_widget.dart
@@ -0,0 +1,88 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+import 'package:stunt_shield_app/app/networking/api_service.dart';
+import 'package:stunt_shield_app/bootstrap/helpers.dart';
+import 'package:stunt_shield_app/resources/themes/text_theme/default_text_theme.dart';
+import 'package:stunt_shield_app/app/models/poster.dart' as Model;
+
+class Poster extends StatefulWidget {
+  Poster({Key? key}) : super(key: key);
+
+  @override
+  _PosterState createState() => _PosterState();
+}
+
+class _PosterState extends NyState<Poster> {
+  @override
+  init() async {
+    super.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NyFutureBuilder(
+        future: _loadPoster(),
+        child: (context, data) {
+          data as List<Model.Poster>;
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  horizontalTitleGap: 0,
+                  title: Text('Poster',
+                      style: defaultTextTheme.titleSmall!.copyWith(
+                          color: ThemeColor.get(context).grey800,
+                          fontWeight: FontWeight.w600)),
+                  trailing: TextButton(
+                    child: Text(
+                      'Lihat Semua',
+                      style: defaultTextTheme.bodySmall!.copyWith(
+                          color: ThemeColor.get(context).yellow,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    onPressed: () {},
+                  ),
+                  leading: SizedBox(
+                    width: 6,
+                    height: 20,
+                    child: DecoratedBox(
+                      decoration:
+                          BoxDecoration(color: ThemeColor.get(context).yellow),
+                    ),
+                  ),
+                ),
+                CarouselSlider(
+                  options:
+                      CarouselOptions(autoPlay: true, enlargeCenterPage: true),
+                  items: data.map((poster) {
+                    return Builder(builder: (BuildContext context) {
+                      return Card(
+                        surfaceTintColor: ThemeColor.get(context).white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            child: Image.network(
+                              poster.thumbnail ?? '-',
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<List<Model.Poster>?> _loadPoster() async {
+    return await api<ApiService>((request) => request.fetchPoster());
+  }
+}
